84cc395959286117d1140e6ae0dfa966b47425a1
Julia <julia.berning@thenotcompany.com>
2024-12-06 21:42:53 -0300

    feat add Models for Topics, Rooms, Messages

--- base/views.py
+++ base/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home page')
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse('Room page')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {"room": room}
+    return render(request, 'base/room.html', context)
