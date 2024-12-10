02154bfea9ef2127c76b0135794582f7b4cbbbf1
Julia <julia.berning@thenotcompany.com>
2024-12-10 20:45:17 -0300

    feat: add topic search and improve post search

--- base/views.py
+++ base/views.py
@@ -64,7 +64,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.all().filter(Q(room__topic__name__icontains=q)) # activity feed can be filtered according to the topic
 
@@ -184,3 +184,8 @@ def updateUser(request,pk):
             form.save()
             return redirect('user-profile', pk=user.id)
     return render(request, 'base/update-user.html', {'form': form})
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
